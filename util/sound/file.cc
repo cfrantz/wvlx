421dcf70b10d114296dc3ba0c458f4e598de26c3
Chris Frantz <frantzcj@gmail.com>
2023-10-20 11:40:16 -0700

    Migrate the sndfile loader to protobufs
    1. Load audio samples into a protobuf message.
    
    Signed-off-by: Chris Frantz <frantzcj@gmail.com>

--- util/sound/file.cc
+++ util/sound/file.cc
@@ -1,111 +1,25 @@
 #include "util/sound/file.h"
 
+#include <sndfile.h>
+
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <sndfile.h>
 
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/match.h"
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "google/protobuf/text_format.h"
 
 namespace sound {
 
 constexpr float PI = 3.14159265358979323846f;
 constexpr double EPSILON = 0.000001;
-
-Channel::Channel(const float* data, size_t samples, double rate, size_t stride)
-  : data_(samples),
-  interp_(Interpolation::None),
-  rate_(rate),
-  length_(samples / rate) {
-    for(size_t i=0; i<samples; ++i) {
-        data_[i] = *data;
-        data += stride;
-    }
-}
-
-Channel Channel::ConstantValue(size_t samples, double rate, float value) {
-    Channel cv = Channel(samples, rate);
-    for(size_t i=0; i<samples; ++i) {
-        cv.data_[i] = value;
-    }
-    return cv;
-}
-
-
-void Channel::Resample(double rate) {
-    std::vector<float> data(length_ * rate);
-    for (size_t i=0; i<data.size(); ++i) {
-        double tm = double(i) / rate;
-        data[i] = this->at(tm);
-    }
-    data_ = data;
-    rate_ = rate;
-}
-
-float Channel::Peak(size_t s0, size_t s1) {
-    float peak = 0.0;
-    for(size_t i=s0; i<s1; ++i) {
-        float val = fabsf(data_[i]);
-        peak = std::fmaxf(peak, val);
-    }
-    return peak;
-}
-
-float Channel::at(double tm) const {
-    if (tm < 0.0 || tm >= length_) return 0.0;
-    double n = tm * rate_;
-    double f = n - floor(n);
-    size_t x = n;
-
-    Interpolation i = interp_;
-    if (f < EPSILON) i = Interpolation::None;
-    if (i == Interpolation::Cubic && (x<1 || x > data_.size()-3)) {
-        i = Interpolation::Cosine;
-    }
-
-    switch(i) {
-        case Interpolation::None:
-            return data_[x];
-        case Interpolation::Linear:
-            return interp_linear(x, f);
-        case Interpolation::Cosine:
-            return interp_cosine(x, f);
-        case Interpolation::Cubic:
-            return interp_cubic(x, f);
-    }
-    return 0.0f;
-}
-
-float Channel::interp_linear(size_t x, double f) const {
-    float y1 = data_[x];
-    float y2 = data_[x+1];
-    return y1 * (1.0 - f) + y2 * f;
-}
-
-float Channel::interp_cosine(size_t x, double f) const {
-    float y1 = data_[x];
-    float y2 = data_[x+1];
-    float mu = (1.0 - cosf(f * PI)) / 2.0;
-    return y1 * (1.0 - mu) + y2 * mu;
-}
-
-float Channel::interp_cubic(size_t x, double f) const {
-    float y0 = data_[x-1];
-    float y1 = data_[x];
-    float y2 = data_[x+1];
-    float y3 = data_[x+2];
-
-    // Cubic interpolation:                                                  
-    //                                                                       
-    // [(-1/2)p0 + (3/2)p1 = (3/2)p2 + (1/2)p3] * x^3                        
-    // + [p0 - (5/2)p1 + 2p2 - (1/2)p3) * x^2                                
-    // + [-(1/2)p0 + (1/2)p2] * x                                            
-    // + p1                                                                  
-    return y1 + 0.5 * f*(y2 - y0 + f*(2.0*y0 - 5.0*y1 + 4.0*y2 - y3 + f*(3.0*(y1-y2) + y3 - y0)));
-}
 
 absl::StatusOr<std::unique_ptr<File>> File::Load(const std::string& filename) {
     SF_INFO info;
@@ -114,110 +28,131 @@ absl::StatusOr<std::unique_ptr<File>> File::Load(const std::string& filename) {
         LOG(ERROR) << "File::Load could not open " << filename;
         return absl::InvalidArgumentError(sf_strerror(fp));
     }
-    auto data = SFRead(fp, &info);
-    sf_close(fp);
-
-    auto file = absl::make_unique<File>();
-    file->info_ = info;
     LOG(INFO) << "soundfile:"
-        << " frames:" << info.frames
-        << " rate:" << info.samplerate
-        << " ch:" << info.channels
-        << " format:" << info.format;
+              << " frames:" << info.frames << " rate:" << info.samplerate
+              << " ch:" << info.channels << " format:" << info.format;
 
-    for(int i=0; i<info.channels; i++) {
-        file->channel_.emplace_back(new Channel(data.data()+i,
-                                                info.frames,
-                                                info.samplerate,
-                                                info.channels));
+    std::vector<float> buf(size_t(info.samplerate * info.channels));
+    size_t count;
+    proto::File* data = new proto::File;
+    data->set_name(filename);
+    data->set_rate(double(info.samplerate));
+    for (int ch = 0; ch < info.channels; ++ch) {
+        data->mutable_channel()->Add();
     }
-    return file;
+    do {
+        count = sf_readf_float(fp, buf.data(), info.samplerate);
+        for (size_t i = 0, n = 0; n < count; ++n) {
+            for (int ch = 0; ch < info.channels; ++ch) {
+                data->mutable_channel(ch)->mutable_data()->Add(buf[i++]);
+            }
+        }
+    } while (count);
+    sf_close(fp);
+    return absl::make_unique<File>(data, true);
 }
 
-absl::StatusOr<std::unique_ptr<File>> File::LoadAsMono(const std::string& filename) {
-    SF_INFO info;
-    SNDFILE* fp = sf_open(filename.c_str(), SFM_READ, &info);
+absl::Status File::SaveProto(const std::string& filename,
+                             const proto::File& file) {
+    std::fstream out(filename, std::ios_base::out | std::ios_base::trunc |
+                                   std::ios_base::binary);
+    if (!file.SerializeToOstream(&out)) {
+        return absl::InternalError("serialization failed");
+    }
+    return absl::OkStatus();
+}
+
+absl::Status File::SaveText(const std::string& filename,
+                            const proto::File& file) {
+    FILE* fp = fopen(filename.c_str(), "wt");
     if (fp == nullptr) {
-        LOG(ERROR) << "File::Load could not open " << filename;
-        return absl::InvalidArgumentError(sf_strerror(fp));
+        return absl::InternalError("failed to open file");
     }
-    auto data = SFRead(fp, &info);
-    sf_close(fp);
-
-    auto file = absl::make_unique<File>();
-    file->info_ = info;
-    LOG(INFO) << "soundfile:"
-        << " frames:" << info.frames
-        << " rate:" << info.samplerate
-        << " ch:" << info.channels
-        << " format:" << info.format;
-
-    // Sum and average all channels into channel 0
-    for(int i=0; i<info.frames; i++) {
-        int k = i * info.channels;
-        for(int j=1; j<info.channels; j++) {
-            data[k] += data[k+j];
-        }
-        data[k] /= float(info.channels);
+    google::protobuf::io::FileOutputStream out(fileno(fp));
+    if (!google::protobuf::TextFormat::Print(file, &out)) {
+        return absl::InternalError("serialization failed");
     }
-
-    // Data still strided as channels, so tell Channel about the stride.
-    file->channel_.emplace_back(new Channel(data.data(),
-                                            info.frames,
-                                            info.samplerate,
-                                            info.channels));
-    return file;
+    return absl::OkStatus();
 }
 
-absl::Status File::Save(const std::string& filename) {
-    if (channel_.empty()) {
-        return absl::FailedPreconditionError("no channels");
-    }
-    SF_INFO info{(sf_count_t)channel_.at(0)->size(),
-                 (int)channel_.at(0)->rate(),
-                 (int)channel_.size(),
-                 SF_FORMAT_WAV | SF_FORMAT_FLOAT};
-    for(const auto& c : channel_) {
-        if (c->size() != (size_t)info.frames) {
+absl::Status File::SaveWav(const std::string& filename,
+                           const proto::File& file) {
+    SF_INFO info{(sf_count_t)file.channel(0).data_size(), (int)file.rate(),
+                 (int)file.channel_size(), SF_FORMAT_WAV | SF_FORMAT_FLOAT};
+    for (const auto& ch : file.channel()) {
+        if (ch.data_size() != info.frames) {
             return absl::FailedPreconditionError(
-                    "Not all channels the same length");
-        }
-        if (c->rate() != info.samplerate) {
-            return absl::FailedPreconditionError(
-                    "Not all channels the same samplerate");
+                "Not all channels the same length");
         }
     }
     LOG(INFO) << "soundfile:"
-        << " frames:" << info.frames
-        << " rate:" << info.samplerate
-        << " ch:" << info.channels
-        << " format:" << info.format;
-
+              << " frames:" << info.frames << " rate:" << info.samplerate
+              << " ch:" << info.channels << " format:" << info.format;
     LOG(INFO) << "format_check " << sf_format_check(&info);
-    SNDFILE *fp = sf_open(filename.c_str(), SFM_WRITE, &info);
+
+    // Take the dumb way out: allocate a buffer big enough to interleave
+    // all of the sample data.
+    std::vector<float> data;
+    for (int i = 0; i < info.frames; ++i) {
+        for (int ch = 0; ch < info.channels; ++ch) {
+            data.emplace_back(file.channel(ch).data(i));
+        }
+    }
+
+    SNDFILE* fp = sf_open(filename.c_str(), SFM_WRITE, &info);
     if (fp == nullptr) {
         return absl::InternalError(sf_strerror(fp));
-    }
-    size_t frames = channel_.size() * channel_.at(0)->size();
-    std::vector<float> data;
-    for(size_t i=0; i<frames; ++i) {
-        for(const auto& c : channel_) {
-            data.emplace_back(c->sample(i));
-        }
     }
     sf_write_float(fp, data.data(), data.size());
     sf_close(fp);
     return absl::OkStatus();
 }
 
-std::vector<float> File::SFRead(SNDFILE* sf, SF_INFO* info) {
-    std::vector<float> data(info->frames * info->channels);
-    size_t count = sf_read_float(sf, data.data(), data.size());
-    if (count != data.size()) {
-        LOG(ERROR) << "SFRead short read: got "
-            << count << " expected " << data.size();
+absl::Status File::Save(const std::string& filename, const proto::File& file) {
+    if (absl::EndsWith(filename, ".wav")) {
+        return SaveWav(filename, file);
+    } else if (absl::EndsWith(filename, ".pb")) {
+        return SaveProto(filename, file);
+    } else if (absl::EndsWith(filename, ".textpb")) {
+        return SaveText(filename, file);
+    } else {
+        return absl::InvalidArgumentError("bad file extension");
     }
-    return data;
 }
 
-}  // namespace
+absl::Status File::Save(const std::string& filename) const {
+    return Save(filename, *file_);
+}
+
+void File::ConvertToMono() {
+    if (file_->channel_size() == 1) {
+        // File is already mono.
+        return;
+    }
+    proto::File* file = new proto::File;
+    file->set_name(file_->name());
+    file->set_rate(file_->rate());
+    auto* data = file->mutable_channel()->Add();
+    float n = file_->channel_size();
+    for (int i = 0; i < file_->channel(0).data_size(); ++i) {
+        float sample = 0;
+        for (int ch = 0; ch < file_->channel_size(); ++ch) {
+            sample += file_->channel(ch).data(i);
+        }
+        data->mutable_data()->Add(sample / n);
+    }
+    replace(file);
+}
+
+float File::Peak(int ch, int s0, int s1) const {
+    auto& channel = file_->channel(ch);
+    float peak = 0.0f;
+    for (int i = s0; i < s1; ++i) {
+        float sample =
+            (i >= 0 && i < channel.data_size()) ? channel.data(i) : 0.0f;
+        peak = std::max(peak, std::abs(sample));
+    }
+    return peak;
+}
+
+}  // namespace sound
--- util/sound/file.h
+++ util/sound/file.h
@@ -1,13 +1,15 @@
 #ifndef WVLX_UTIL_SOUND_FILE_H
 #define WVLX_UTIL_SOUND_FILE_H
+#include <sndfile.h>
+
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
-#include <cstdint>
-#include <sndfile.h>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "proto/sound.pb.h"
 
 namespace sound {
 
@@ -18,66 +20,60 @@ enum class Interpolation {
     Cubic,
 };
 
-class Channel {
-  public:
-    Channel(const float* data, size_t samples, double rate, size_t stride=1);
-    Channel(size_t samples, double rate)
-      : data_(samples),
-       rate_(rate),
-       length_(samples / rate) {}
-
-    static Channel ConstantValue(size_t samples, double rate, float value);
-
-    void Resample(double rate);
-    float Peak(size_t s0, size_t s1);
-    float at(double tm) const;
-    // Gets the sample at index, mirrors signal at the endpoints.
-    inline float sample(int64_t index) const {
-        int64_t len = int64_t(data_.size());
-        if (index < 0) {
-            return -sample(-index);
-        } else if (index >= len) {
-            index = (len - 1) - (index - len);
-            return -sample(-index);
-        } else {
-            return data_.at(index);
-        }
-    }
-    inline float* data() { return data_.data(); }
-    inline size_t size() const { return data_.size(); }
-    inline double rate() const { return rate_; }
-    inline double length() const { return length_; }
-    inline void resize(size_t samples) { data_.resize(samples); }
-    inline void set_interpolation(Interpolation i) { interp_ = i; }
-
-  private:
-    float interp_linear(size_t x, double f) const;
-    float interp_cosine(size_t x, double f) const;
-    float interp_cubic (size_t x, double f) const;
-
-    std::vector<float> data_;
-    Interpolation interp_;
-    double rate_;
-    double length_;
-};
-
 class File {
   public:
     File() = default;
-    static absl::StatusOr<std::unique_ptr<File>> Load(const std::string& filename);
-    static absl::StatusOr<std::unique_ptr<File>> LoadAsMono(const std::string& filename);
-    absl::Status Save(const std::string& filename);
+    File(proto::File* file, bool take_ownership = false)
+        : file_(file), owning_(take_ownership) {}
+    virtual ~File() {
+        if (owning_) delete file_;
+    }
+    static absl::StatusOr<std::unique_ptr<File>> Load(
+        const std::string& filename);
+    static absl::Status Save(const std::string& filename,
+                             const proto::File& file);
+    static absl::Status SaveWav(const std::string& filename,
+                                const proto::File& file);
+    static absl::Status SaveProto(const std::string& filename,
+                                  const proto::File& file);
+    static absl::Status SaveText(const std::string& filename,
+                                 const proto::File& file);
+    absl::Status Save(const std::string& filename) const;
+    void ConvertToMono();
 
-    inline size_t channels() { return channel_.size(); }
-    std::shared_ptr<Channel> channel(size_t i) { return channel_.at(i); }
-    void add_channel(std::shared_ptr<Channel> c) { channel_.emplace_back(c); }
-    const SF_INFO& info() { return info_; }
+    int channels() const { return file_->channel_size(); }
+    int size() const { return file_->channel(0).data_size(); }
+    double rate() const { return file_->rate(); }
+    double length() const { return size() / rate(); }
+
+    float Sample(int ch, int i) const {
+        if (i < 0) {
+            return -Sample(ch, -i);
+        } else if (i >= file_->channel(ch).data_size()) {
+            i = (file_->channel(ch).data_size() - 1) -
+                (i - file_->channel(ch).data_size());
+            return -Sample(ch, i);
+        } else {
+            return file_->channel(ch).data(i);
+        }
+    }
+    float Sample(int ch, double i) const { return Sample(ch, int(rate() * i)); }
+    float Peak(int ch, int s0, int s1) const;
+
+    const proto::File& data() { return *file_; }
+    proto::File* release() {
+        owning_ = false;
+        return file_;
+    };
+    void replace(proto::File* f) {
+        if (owning_) delete file_;
+        file_ = f;
+    }
 
   private:
-    static std::vector<float> SFRead(SNDFILE* sf, SF_INFO* info);
-    std::vector<std::shared_ptr<Channel>> channel_;
-    SF_INFO info_;
+    proto::File* file_ = nullptr;
+    bool owning_ = false;
 };
 
-}  // namespace
-#endif // WVLX_UTIL_SOUND_FILE_H
+}  // namespace sound
+#endif  // WVLX_UTIL_SOUND_FILE_H
